97505b093a0479d883e69c847a6d3ede96671157
fordimalanda <fordimalanda7@gmail.com>
2025-05-18 19:47:01 +0200

    Création de la class Matrix et le constructeur

--- src/main/java/com/fordimalanda/matrix/matrix.java
+++ src/main/java/com/fordimalanda/matrix/matrix.java
@@ -1,1 +1,12 @@
 package com.fordimalanda.matrix;
+
+public class Matrix {
+    private int a1, a2, a3, a4;
+
+    public Matrix(int a1, int a2, int a3, int a4) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+        this.a4 = a4;
+    }
+}
