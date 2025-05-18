419884cbd69f27b6996a720d1c7c7d22bd37084e
fordimalanda <fordimalanda7@gmail.com>
2025-05-18 20:50:38 +0200

    L'affichage pour Printer

--- src/main/java/com/fordimalanda/matrix/MatrixPrinter.java
+++ src/main/java/com/fordimalanda/matrix/MatrixPrinter.java
@@ -1,1 +1,16 @@
-package com.fordimalanda.matrix;package com.fordimalanda.matrix;
+package com.fordimalanda.matrix;
+
+public class MatrixPrinter {
+    public static void printMatrix(Matrix matrix, String name) {
+        System.out.println("\nMatrice " + name + ":");
+        System.out.println("( " + matrix.getA1() + "     " + matrix.getA2() + " )");
+        System.out.println("( " + matrix.getA3() + "     " + matrix.getA4() + " )");
+    }
+
+    public static void printMultiplication(Matrix m1, Matrix m2) {
+        System.out.println("\nReprésentation de l'opération:");
+        System.out.println("( " + m1.getA1() + "     " + m1.getA2() + " )\t     ( " + m2.getA1() + "     " + m2.getA2() + " )");
+        System.out.println("\t\tx");
+        System.out.println("( " + m1.getA3() + "     " + m1.getA4() + " )\t     ( " + m2.getA3() + "     " + m2.getA4() + " )");
+    }
+}
